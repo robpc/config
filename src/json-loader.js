bb35d04f3ae3022e96bdde5389bbf31139f34d51
Rob Cannon <rpcann@gmail.com>
2020-09-17 13:32:39 -0400

    Improved debugging

--- src/json-loader.js
+++ src/json-loader.js
@@ -17,9 +17,10 @@ const fs = require('fs');
 const Config = require('./core');
 const Logger = require('./logger');
 
-const { NODE_ENV } = process.env;
-
 const logger = new Logger('config-json-loader');
+
+const { NODE_ENV } = process.env;
+logger.log(`Environment (NODE_ENV): ${NODE_ENV}`);
 
 const configNames = [
   'default',
@@ -41,17 +42,21 @@ const configs = configNames
       try {
         const str = fs.readFileSync(jsonFilename);
         const json = JSON.parse(str);
-        loadedFiles.push(`${name} -> ${jsonFilename}`);
+        loadedFiles.push(`${name} -> ${jsonFilename} [LOADED]`);
         return json;
       } catch (err) {
         logger.error(`Problem reading config file '${jsonFilename}`, err);
         return null;
       }
+    } else {
+      loadedFiles.push(`${name} -> ${jsonFilename} [not found]`);
     }
 
     return null;
   })
   .filter((config) => config);
+logger.log('Loading config from files:');
+loadedFiles.forEach((s) => logger.log(` - ${s}`));
 
 const merged = {};
 
@@ -59,8 +64,6 @@ configs.forEach((conf) => Object.assign(merged, conf));
 
 const finalConfig = merged;
 
-logger.log('Loading config from files:');
-loadedFiles.forEach((s) => logger.log(` - ${s}`));
 logger.log('config:', JSON.stringify(finalConfig, null, 2));
 
 module.exports = new Config(finalConfig);
